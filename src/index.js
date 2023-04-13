a7b49e1f2647346bdd4ac1b7a57d655d6d9e6d6d
bartekkowalski465 <bartekkowalski925@gmail.com>
2023-04-13 19:49:10 -0400

    I think I found the problem

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Account from './components/Account';
+import Home from './components/Home';
 
-ReactDOM.render(
-  <BrowserRouter basename="/buildspace">
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+
+const Router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/home", element: <Home /> },
+  { path: "/account", element: <Account /> },
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <RouterProvider router={Router}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
