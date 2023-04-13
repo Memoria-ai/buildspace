4ab06046296796082f33acf2293776d648ef8a23
bartekkowalski465 <bartekkowalski925@gmail.com>
2023-04-13 19:55:20 -0400

    here

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,7 +15,7 @@ const Router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/home", element: <Home /> },
   { path: "/account", element: <Account /> },
-], { basename: "/your-base-name" }); // add your base name here
+], { basename: "/buildspace" }); // add your base name here
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
