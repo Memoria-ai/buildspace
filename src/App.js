1570fc5071be1f546e1012519c99c6276b8bbb5e
bartekkowalski465 <bartekkowalski925@gmail.com>
2023-04-13 19:12:16 -0400

    testing app.js upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useEffect } from 'react'
+import { supabase } from './supabaseClient'
+import Auth from './Auth'
+import Account from './components/Account'
+import Home from './components/Home'
+// import { Navigate } from 'react-router-dom'
 
 function App() {
+  const [session, setSession] = useState(null)
+  // const navigate = Navigate();
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+    console.log(session)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React qwhatagain
-        </a>
-      </header>
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      {console.log(session)}
+      {!session ? (
+        // <Auth />
+        <div>here</div>
+      ) : (
+        // <Home session={session} />
+        <div>there</div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
