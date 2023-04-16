f8540a44ceb04bf0c07e51256da1b3748c881ef5
bartekkowalski465 <bartekkowalski925@gmail.com>
2023-04-16 01:36:35 -0400

    fixed redirect

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -25,7 +25,8 @@ export default function Auth() {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'twitter',
       options: {
-        redirectTo: 'http://localhost:3000/buildspace',
+        redirectTo: 'https://memoria-ai.github.io/buildspace/',
+        // http://localhost:3000/buildspace
       }
     })
   }
