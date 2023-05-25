11e9aea59e0acdd793a27f9a69cb21873b7e6944
bartekkowalski465 <bartekkowalski925@gmail.com>
2023-05-24 22:12:44 -0400

    pushing change???? PLEASE WORK I SWEAR TO GOD

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -171,35 +171,33 @@ const Create = ({ session }) =>{
   };
   
   const handleStopRecording = async (blob) => {
-    
-    // const audioBlob = new Blob(chunksRef.current, { type: "audio/mp4" });
-    // console.log(chunksRef.current)
-    // verify the blob is not empty
-    if(blob.size === 0){
+    if (blob.size === 0) {
       return;
     }
     const formData = new FormData();
     formData.append('audio', blob, 'audio.mp4');
+  
     try {
-      const headers = new Headers();
-      headers.append('Content-Type', 'multipart/form-data');
-      const response = await fetch(`${current}audio`, {
+      const response = await fetch(current+'transcribe', {
         method: 'POST',
         body: formData,
-        //headers: headers
-        //credentials: 'include'
       });
   
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
+  
       const data = await response.json();
-      setNote(data.text);
-      await stoppedListeningFunction(data.text);
+      const transcript = data.transcription;
+      console.log('Transcription:', data.transcription);
+      setNote(data.transcription);
+      stoppedListeningFunction(transcript);
     } catch (error) {
-      console.log(error);
+      console.log('Error:', error.message);
     }
+  
     chunksRef.current = [];
+    
   };
 
   const handleDiscardClick = async (event) => {
