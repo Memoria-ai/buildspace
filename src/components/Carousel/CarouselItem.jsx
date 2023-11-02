889db9d362fbcbe81c46b929e63710fedfdd4754
bartek717 <bartekkowalski925@gmail.com>
2023-11-02 00:48:00 -0400

    trying to fix pages

--- src/components/Carousel/CarouselItem.jsx
+++ src/components/Carousel/CarouselItem.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../../pages/Auth/Auth.module.css";
 
 export const CarouselItem = ({ item }) => {
+  // 
   return (
     <div className={styles.carouselItem}>
       <img className={styles.carouselImg} src={item.icon} />
